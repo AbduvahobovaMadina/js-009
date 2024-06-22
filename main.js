42bd3830239607e98ace4552cf5defb947bddd5d
AbduvahobovaMadina <madinaabduvahobova565@gmail.com>
2024-06-23 01:28:55 +0500

    /

--- main.js
+++
@@ -1,9 +0,0 @@
-// console.log("start");
-
-{
-    // 1."Janob" va "Xonim" so'zini mosini qo'yib qaytaring
-   
-    console.log( mrMs({name:"Akmal", gender:"male"})) ;
-    console.log( mrMs({name:"Laylo", gender:"female"})) ;
-    console.log( mrMs({name:"Ozodbek", gender:"male"})) ;
-}
