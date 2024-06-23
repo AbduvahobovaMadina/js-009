7a3ba4cb98a661d5f321383520514424e9b7d457
AbduvahobovaMadina <madinaabduvahobova565@gmail.com>
2024-06-24 00:28:19 +0500

    /

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 {
-    // 1. "janob " va "xonim" manosini qoshib qaytaring
+    // 1 "janob " va "xonim" manosini qoshib qaytaring
     const mrMs = [
       {
         name: "Akmal",
@@ -21,9 +21,9 @@
    ];
     for (let value of mrMs) {
       if (value.gender === "male") {
-        console.log(`janob ${value.name}`);
+        // console.log(`janob ${value.name}`);
       } else {
-        console.log(`xonim ${value.name}`);
+        // console.log(`xonim ${value.name}`);
       }
     }
 }
@@ -37,7 +37,50 @@
         console.log(num);
       }
     
-      console.log(card("29381209809123", true));
-      console.log(card("29381209809123", false));
+    //   console.log(card("9860012106407476", true));
+    //   console.log(card("9860012106407476", false));
+}
+
+{
+    // 3 perimetrini qaytaradigan function tuzing
+    function perimetrRect(a, b){
+        return 2 * (a + b);
+    }
+    // console.log(perimetrRect(6, 6));
+}
+{
+    // 4 masala "Ha" ga true "Yo'q" ga false qaytaring faqat arrow functionda bir qatorda
+    const haYoq = (str) =>
+        str === "Ha" ? console.log(true) : console.log(false);
+    
+    //   console.log(haYoq("Ha"));
+    //   console.log(haYoq("Yo'q"));
+}
+
+
+{
+    // 5 agar + bo'lmasa telni raqamiga + qo'shib qaytaring
+    function tell(n) {
+        console.log(n.replace(n, `+${n}`));
+      }
+    //   console.log(tell("998994310529"));
+}
+
+{
+    // 6 declarition functionni arraw functionga o'giring va bir qatorda yozing
+    {
+        const hi = () => "Hello world";
+    
+        // console.log(hi());
+    }
+}
+
+{
+    // 7 "dark " kiritsa "light " ni qaytaring yoki aksi
+    const brightness = (str) =>
+        str === "light" ? console.log("dark") : console.log("light");
+    
+      console.log(brightness("light"));
+      console.log(brightness("dark"));
 }
 
