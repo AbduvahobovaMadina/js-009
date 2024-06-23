7e005769189e5eb962366a93626ee63c51b8db23
AbduvahobovaMadina <madinaabduvahobova565@gmail.com>
2024-06-23 21:51:20 +0500

    /

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-// console.log("start");
 
 {
-    // 1."Janob" va "Xonim" so'zini mosini qo'yib qaytaring
-   
-    console.log( mrMs({name:"Akmal", gender:"male"})) ;
-    console.log( mrMs({name:"Laylo", gender:"female"})) ;
-    console.log( mrMs({name:"Ozodbek", gender:"male"})) ;
+    // 1. "janob " va "xonim" manosini qoshib qaytaring
+    const mrMs = [
+      {
+        name: "Akmal",
+        gender: "male",
+      },
+      {
+        name: "Laylo",
+        gender: "female",
+      },
+      {
+        name: "Ozod",
+        gender: "male",
+      },
+      {
+        name: "Aisha",
+        gender: "female",
+      },
+   ];
+    for (let value of mrMs) {
+      if (value.gender === "male") {
+        console.log(`janob ${value.name}`);
+      } else {
+        console.log(`xonim ${value.name}`);
+      }
+    }
 }
+
+{
+    // 2 agar ikkinchi argument false bo'lsa carta raqami yashirilsin aks holda aksi
+    function card(num, bool) {
+        if (bool === false) {
+          num = num.slice(0, 4) + "*******";
+        }
+        console.log(num);
+      }
+    
+      console.log(card("29381209809123", true));
+      console.log(card("29381209809123", false));
+}
+
